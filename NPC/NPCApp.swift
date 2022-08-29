3eee343b2f475e7982156603da311f6a73c7240a
Phuong Nguyen <pnha1303@gmail.com>
2022-08-30 00:30:25 +0700

    initial commit

---
+++ NPC/NPCApp.swift
@@ -0,0 +1,29 @@
+//
+//  NPCApp.swift
+//  NPC
+//
+//  Created by Nguyen Huynh Phuong Anh on 29/08/2022.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct NPCApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
