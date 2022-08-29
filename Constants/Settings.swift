0a2c50952bbe60e1a6bb988a763410721fdf36b8
Phuong Nguyen <pnha1303@gmail.com>
2022-08-30 00:33:04 +0700

    feat: files

---
+++ Constants/Settings.swift
@@ -0,0 +1,8 @@
+//
+//  Settings.swift
+//  NPC
+//
+//  Created by Nguyen Huynh Phuong Anh on 30/08/2022.
+//
+
+import Foundation
---
+++ Models/Users.swift
@@ -0,0 +1,8 @@
+//
+//  Users.swift
+//  NPC
+//
+//  Created by Nguyen Huynh Phuong Anh on 30/08/2022.
+//
+
+import Foundation
