fc2473229ec2d77b42f706aa81c2a503085e043c
Nguyen Anh Minh <kullen.177@gmail.com>
2022-09-02 16:25:55 +0700

    Added Transition View

---
+++ Views/TransitionView.swift
@@ -0,0 +1,20 @@
+//
+//  TransitionView.swift
+//  NPC
+//
+//  Created by Le Nguyen on 02/09/2022.
+//
+
+import SwiftUI
+
+struct TransitionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TransitionView_Previews: PreviewProvider {
+    static var previews: some View {
+        TransitionView()
+    }
+}
