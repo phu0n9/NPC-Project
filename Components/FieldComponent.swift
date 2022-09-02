dec01fe83b87cb8d638a730f4136f2a91da608a8
Nguyen Anh Minh <kullen.177@gmail.com>
2022-09-02 15:58:41 +0700

    Nguyen Anh Minh commit

---
+++ Components/FieldComponent.swift
@@ -0,0 +1,20 @@
+//
+//  FieldComponent.swift
+//  NPC
+//
+//  Created by Le Nguyen on 02/09/2022.
+//
+
+import SwiftUI
+
+struct FieldComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FieldComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        FieldComponent()
+    }
+}
---
+++ Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  NPC
+//
+//  Created by Le Nguyen on 02/09/2022.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
