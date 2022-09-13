18c06d02cb9dcbe34f676520b50d1cf9256a9599
Le Nguyen <s3777242>
2022-09-13 21:55:30 +0700

    feat: notification

--- NPC/NPCApp.swift
+++ NPC/NPCApp.swift
@@ -7,12 +7,36 @@
 
 import SwiftUI
 import FirebaseCore
+import Firebase
+import FirebaseMessaging
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        
+        // Notification stuff
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        
+        // Request authorization
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]){success,_ in
+            guard success else {
+                return
+            }
+            print ("Success in APNS registry")
+        }
+        application.registerForRemoteNotifications()
         return true
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        messaging.token { token, _ in
+            guard let token = token else {
+                return
+            }
+            print("Token: \(token)")
+        }
     }
 }
 
