61d64797ca4a982b5ef2719e957453940307e202
Le Nguyen <s3777242>
2022-09-14 20:39:25 +0700

    added notification manager

---
+++ Helpers/NotificationManager.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationManager.swift
+//  NPC
+//
+//  Created by Le Nguyen on 14/09/2022.
+//
+
+import SwiftUI
+
+struct NotificationManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationManager_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationManager()
+    }
+}
